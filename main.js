e2bb94e1ded65b8cdd26847c9a076f1686811854
Natassja <natassja.davila@gmail.com>
2022-10-17 15:27:10 -0300

    versao final

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ $(document).ready(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
         const enderecoNovaImagem = $('#endereco-nova-imagem').val();
-        
+        const novoItem = $('<li></li>');
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);  
+        $(`<div class="overlay-imagem-link">
+        <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a></div>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul'); 
+        $(novoItem).fadeInd(1000);
+        $('#endereco-imagem-nova').val('');       
     })
 })
 
